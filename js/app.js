e689ce276b91cc2f3fc495335ad8edc42d541848
Brandt Bowling <Brandt Bowling>
2016-09-02 23:18:32 -0400

    still need to set up ng-routes

---
+++ js/app.js
@@ -0,0 +1,22 @@
+var app = angular.
+
+module("SocialSkills", [])
+.controller('DataCollectionController', ['$scope', function($scope) {
+    $scope.clientName = [
+        {
+            name: 'Kid1'
+        },
+        {
+            name: 'Kid2'
+        },
+        {
+            name: 'Kid3'
+        },
+        {
+            name: 'Kid4'
+        },
+        {
+            name: 'Kid5'
+        },
+    ]
+}]);
