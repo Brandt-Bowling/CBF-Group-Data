d6eff080a866e286a9657ee4f0bf28a0bb2c7c0a
Brandt Bowling <Brandt Bowling>
2016-09-27 12:53:38 -0400

    think I got the data collection stuff down! working on css a bit

--- js/controllers/dcController.js
+++ js/controllers/dcController.js
@@ -1,58 +1,45 @@
 app.controller('DataCollectionController', ['$scope', function($scope) {
     $scope.clients = [
-        {
-            name: ''
-        },
-        {
-            name: ''
-        },
-        {
-            name: ''
-        },
-        {
-            name: ''          
-        },
-        {
-            name: ''
-        }
+        {name: ''},
+        {name: ''},
+        {name: ''},
+        {name: ''},
+        {name: ''}
     ];
 
-    $scope.totals = [
-        {nameID: 0},
-        {peerInteraction: 0},
-        {groupInstruction: 0},
-        {groupWithDisc: 0},
-        {answerQuestion: 0},
-        {participateSong: 0}
-    ];
-
-    $scope.positiveInteraction = function(person, index, prop, total) {
+    $scope.positiveInteraction = function(person, index, prop) {
         console.log('Positive interaction in', prop,'with', person.name, index);
+
+        var propTotal = prop + 'total';
 
         if(!person.hasOwnProperty(prop)) {
             person[prop] = 1;
+            person[propTotal] = 1;
         } else {
             person[prop]++;
+            person[propTotal]++;
         }
-        $scope.totals[total]++;
-        console.log('total=', $scope.totals[total]);
+        console.log('total in', prop, '=', person[propTotal]);
     }
 
     $scope.negativeInteraction = function(person, index, prop) {
         console.log('Missed interaction in', prop, 'with', person.name, index);
 
+        var propTotal = prop + 'total';
+
         if(!person.hasOwnProperty(prop)) {
             person[prop] = -1;
+            person[propTotal] = -1;
         } else {
             person[prop]--;
+            person[propTotal]++;
         }
-        person.totals[prop]++;
-        console.log('total=', person.totals[prop]);
+        console.log('total in', prop, '=', person[propTotal]);
     }
 
     $scope.collectData = true;
 
     $scope.showData = function() {
-        collectData = false;
+        $scope.collectData = false;
     };
 }]);
