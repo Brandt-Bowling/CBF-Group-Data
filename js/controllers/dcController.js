5cbb4dd1698a75fb3bbeceb430353a49d6046bfc
Brandt Bowling <Brandt Bowling>
2016-09-28 13:42:22 -0400

    Got the view/hide working and successfully display data

--- js/controllers/dcController.js
+++ js/controllers/dcController.js
@@ -10,7 +10,7 @@ app.controller('DataCollectionController', ['$scope', function($scope) {
     $scope.positiveInteraction = function(person, index, prop) {
         console.log('Positive interaction in', prop,'with', person.name, index);
 
-        var propTotal = prop + 'total';
+        var propTotal = prop + 'Total';
 
         if(!person.hasOwnProperty(prop)) {
             person[prop] = 1;
@@ -25,13 +25,12 @@ app.controller('DataCollectionController', ['$scope', function($scope) {
     $scope.negativeInteraction = function(person, index, prop) {
         console.log('Missed interaction in', prop, 'with', person.name, index);
 
-        var propTotal = prop + 'total';
+        var propTotal = prop + 'Total';
 
         if(!person.hasOwnProperty(prop)) {
-            person[prop] = -1;
-            person[propTotal] = -1;
+            person[prop] = 0;
+            person[propTotal] = 1;
         } else {
-            person[prop]--;
             person[propTotal]++;
         }
         console.log('total in', prop, '=', person[propTotal]);
