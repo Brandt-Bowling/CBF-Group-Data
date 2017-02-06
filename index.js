a181118876e4b028514b9891399e83d5b614df68
Brandt Bowling <Brandt Bowling>
2017-02-06 11:29:46 -0500

    changes to index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ var authCheck = jwt({
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/app'));
+app.use(express.static(__dirname));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
